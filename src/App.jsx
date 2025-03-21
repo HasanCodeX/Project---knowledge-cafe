8eb0c6d7149fb3c19cfc62c0114025a6aa776e24
hasancodex <dev.hasan10@gmail.com>
2025-03-22 02:52:27 +0600

    install react and tailwind

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     
+      <h1 className='text-3xl font-bold underline text-blue-400'>knowledge-cafe</h1>
+     
+     
+    </>
+  )
+}
+
+export default App
